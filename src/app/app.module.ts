cbdfc435f0cae48b55b1e07c7e1f8103248870ff
jorgeDonoso <jrg.donoso@gmail.com>
2020-07-15 14:52:34 -0400

    update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {ReactiveFormsModule} from '@Angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 
